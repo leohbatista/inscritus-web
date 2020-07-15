524831b0911d90b1fe6707c88927538690adb848
Leonardo H. Batista <leohbatista@users.noreply.github.com>
2020-07-14 22:03:31 -0300

    Fixes

--- src/app/containers/user-side/my-qrcode/my-qrcode.component.ts
+++ src/app/containers/user-side/my-qrcode/my-qrcode.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { User } from 'functions/src/users/user.model';
@@ -14,15 +15,26 @@ export class MyQrcodeComponent implements OnInit, OnDestroy {
   user: User;
   isLoading = true;
 
+  mobileQuery: MediaQueryList;
+  private mobileQueryListener: () => void;
+
   constructor(
     private authService: AuthService,
-  ) { }
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 350px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
 
   ngOnInit(): void {
     this.userSubscription = this.authService.user.subscribe(user => {
       this.user = user;
       this.isLoading = false;
     });
+
+    this.mobileQuery.removeListener(this.mobileQueryListener);
   }
 
   ngOnDestroy(): void {
