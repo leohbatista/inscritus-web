a76431f98aac3dd35d0e378ff1447c303ea100a4
Leonardo H. Batista <leohbatista@users.noreply.github.com>
2020-07-19 21:13:56 -0300

    Started Activities List

--- src/app/containers/admin-side/activity-list/activity-list.component.ts
+++ src/app/containers/admin-side/activity-list/activity-list.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LocationsService } from 'src/app/services/locations.service';
+import { AdminActivitiesService } from 'src/app/services/admin-activities.service';
+import { Activity, ActivityType } from 'functions/src/activities/activity.model';
+import { Location } from 'functions/src/locations/location.model';
+import { Subscription } from 'rxjs';
+import * as _ from 'lodash';
+import { SpeakersService } from 'src/app/services/speakers.service';
+import { Speaker } from 'functions/src/speakers/speaker.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-activity-list',
   templateUrl: './activity-list.component.html',
   styleUrls: ['./activity-list.component.scss']
 })
-export class ActivityListComponent implements OnInit {
+export class ActivityListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  activities: Activity[];
+  locations: Location[];
+  speakers: Speaker[];
+  types: ActivityType[];
+
+  activitiesSubscription: Subscription;
+  activityTypesSubscription: Subscription;
+  locationsSubscription: Subscription;
+  speakersSubscription: Subscription;
+
+  constructor(
+    private activitiesAdmin: AdminActivitiesService,
+    private locationsService: LocationsService,
+    private speakersService: SpeakersService,
+  ) { }
 
   ngOnInit(): void {
+    this.activitiesSubscription = this.activitiesAdmin.getActivities().subscribe(activities => {
+      this.activities = _.sortBy(activities, ['name']);
+    });
+
+    this.activityTypesSubscription = this.activitiesAdmin.getActivityTypes().subscribe(types => {
+      this.types = types;
+    });
+
+    this.locationsSubscription = this.locationsService.getLocations().subscribe(locations => {
+      this.locations = locations;
+    });
+
+    this.speakersSubscription = this.speakersService.getSpeakers().subscribe(speakers => {
+      this.speakers = speakers;
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.activitiesSubscription) { this.activitiesSubscription.unsubscribe(); }
+    if (this.activityTypesSubscription) { this.activityTypesSubscription.unsubscribe(); }
+    if (this.locationsSubscription) { this.locationsSubscription.unsubscribe(); }
+    if (this.speakersSubscription) { this.speakersSubscription.unsubscribe(); }
+  }
+
+  getActivityType(typeId: string): string {
+    return _.find(this.types, ['id', typeId])?.name || '-';
+  }
+
+  getLocation(locationId: string): string {
+    return _.find(this.locations, ['id', locationId])?.name || '-';
+  }
+
+  getSpeakers(activity: Activity): string[] {
+    return activity.speakers?.map(s => _.find(this.speakers, ['id', s])?.name || '');
+  }
+
+  getDateTime(activity: Activity) {
+    const startDate = moment(activity.startDate);
+    const endDate = activity.endDate ? moment(activity.endDate) : null;
+
+    console.log(startDate, endDate);
+
+
+    return `${startDate.format('DD/MM/YYYY')} ${activity.startTime || ''}` +
+      (!!endDate ? ` - ${endDate.format('DD/MM/YYYY')} ${activity.endTime || ''}` : '');
+  }
 }
